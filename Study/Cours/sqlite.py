f4ce2c62f866b23d1168078b4c4e139927317414
trungbui2307 <trungbuias@gmail.com>
2021-01-18 15:21:44 +0100

    UPDATE

--- Study/Cours/sqlite.py
+++ Study/Cours/sqlite.py
@@ -29,8 +29,28 @@ conn.commit()
 c.execute('UPDATE books SET title="New Book" WHERE rowid = 2')
 conn.commit()
 
-update_book(book, "New Title")
+#update_book(book, "New Title")
 
 c.execute('SELECT * FROM books')
 data = c.fetchall()
 print(data)
+
+### CREATE AN SDK ###
+
+import booksSDK
+from book import Book
+
+book = Book("Are You My Mother?", 1000)
+print("Added book id:", booksSDK.add_book(book))
+print("Get book by title:", booksSDK.get_book_by_title(book.title))
+print("Not a valid book:", booksSDK.get_book_by_title("yeet"))
+
+booksSDK.add_book(Book('The Digging-est Dog', 76))
+print("get books:", booksSDK.get_books())
+
+book = booksSDK.update_book(book, book.title, 76)
+print("Updated book:", book)
+print("Deleted:", booksSDK.delete_book(book))
+print("After delete:", booksSDK.get_book_by_title("Are You My Mother?"))
+
+print("All books:", booksSDK.get_books())
